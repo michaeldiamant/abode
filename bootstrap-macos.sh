d9f2cd633990edd5052a1303163bcab39afe1a1d
michaeldiamant <michaeldiamant@users.noreply.github.com>
2022-02-24 20:28:02 -0500

    Install ohmyzsh

--- bootstrap-macos.sh
+++ bootstrap-macos.sh
@@ -2,6 +2,19 @@
 
 set -euo pipefail
 
+function installOhMyZsh() {
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+  brew install romkatv/powerlevel10k/powerlevel10k
+  # echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+
+  brew install zsh-syntax-highlighting
+  # echo "source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >>~/.zshrc
+
+  brew install zsh-autosuggestions
+  # echo "source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >>~/.zshrc
+}
+
 function installGo() {
   local goVersion="$1"
 
@@ -43,6 +56,7 @@ function installAsdfPackages() {
   asdf global python 3.10.2 2.7.18
 }
 
+installOhMyZsh
 installScmBreeze
 
 brew tap kidonng/malt # https://github.com/dexterleng/vimac/issues/152#issuecomment-903099562
