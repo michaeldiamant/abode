e9ddc15dbabaee663cb4a164fe09929a5dbe00e3
michaeldiamant <michaeldiamant@users.noreply.github.com>
2023-05-26 11:13:21 -0400

    Add descriptions to Telescope keymaps

--- dotfiles/nvim/.config/nvim/after/plugin/trouble.lua
+++ dotfiles/nvim/.config/nvim/after/plugin/trouble.lua
@@ -49,20 +49,20 @@ require("trouble").setup({
 })
 
 vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
-  {silent = true, noremap = true, desc = "Toggle trouble"}
+  {silent = true, noremap = true, desc = "Toggle Trouble"}
 )
 vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
-  {silent = true, noremap = true, desc = "Toggle trouble workspace diagnostics"}
+  {silent = true, noremap = true, desc = "Toggle Trouble workspace diagnostics"}
 )
 vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
-  {silent = true, noremap = true, desc = "Toggle trouble document diagnostics"}
+  {silent = true, noremap = true, desc = "Toggle Trouble document diagnostics"}
 )
 vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
-  {silent = true, noremap = true, desc = "Toggle trouble location list"}
+  {silent = true, noremap = true, desc = "Toggle Trouble location list"}
 )
 vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
-  {silent = true, noremap = true, desc = "Toggle trouble quickfix list"}
+  {silent = true, noremap = true, desc = "Toggle Trouble quickfix list"}
 )
 vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
-  {silent = true, noremap = true, desc = "Toggle trouble LSP references"}
+  {silent = true, noremap = true, desc = "Toggle Trouble LSP references"}
 )
