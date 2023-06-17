38758f6dd7800bdd9e8732e04d9362787a5aaaf9
michaeldiamant <michaeldiamant@users.noreply.github.com>
2023-06-16 23:09:33 -0400

    Update octo

--- dotfiles/nvim/nvim/after/plugin/octo.lua
+++ dotfiles/nvim/nvim/after/plugin/octo.lua
@@ -1,5 +1,6 @@
 require("octo").setup({
   use_local_fs = true,                    -- use local files on right side of reviews
+  enable_builtin = true,                  -- shows a list of builtin actions when no action is provided
   default_remote = {"upstream", "origin"}; -- order to try remotes
   ssh_aliases = {},                        -- SSH aliases. e.g. `ssh_aliases = {["github.com-work"] = "github.com"}`
   reaction_viewer_hint_icon = "";         -- marker for user reactions
