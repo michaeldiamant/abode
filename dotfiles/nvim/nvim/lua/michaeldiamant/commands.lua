9845065c8bfa9afefac0c1b3a508a741b065f048
michaeldiamant <michaeldiamant@users.noreply.github.com>
2023-12-20 10:40:35 -0500

    Refresh stale nvim buffers

--- dotfiles/nvim/nvim/lua/michaeldiamant/commands.lua
+++ dotfiles/nvim/nvim/lua/michaeldiamant/commands.lua
@@ -5,3 +5,6 @@ vim.cmd[[au TextYankPost * silent! lua vim.highlight.on_yank({timeout=500})]]
 -- * Source: https://vim.fandom.com/wiki/Auto_save_files_when_focus_is_lost
 -- * For tmux integration: `set -g focus-events on`
 vim.cmd[[:au FocusLost * :wa]]
+
+-- Refresh stale buffers when file system changed outside of nvim.
+vim.cmd[[:set autoread]]
