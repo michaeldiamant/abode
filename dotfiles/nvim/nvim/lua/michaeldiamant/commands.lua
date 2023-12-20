065951f5778608001defd696bf629a6c992e6555
michaeldiamant <michaeldiamant@users.noreply.github.com>
2023-12-20 10:34:41 -0500

    Auto-save nvim on focus lost

--- dotfiles/nvim/nvim/lua/michaeldiamant/commands.lua
+++ dotfiles/nvim/nvim/lua/michaeldiamant/commands.lua
@@ -1,2 +1,7 @@
 -- Highlight yanked text.
 vim.cmd[[au TextYankPost * silent! lua vim.highlight.on_yank({timeout=500})]]
+
+-- Save all buffers on focus lost.
+-- * Source: https://vim.fandom.com/wiki/Auto_save_files_when_focus_is_lost
+-- * For tmux integration: `set -g focus-events on`
+vim.cmd[[:au FocusLost * :wa]]
