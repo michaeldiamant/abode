4444fe4daa295a301d7312dc67aba1562978e5b7
michaeldiamant <michaeldiamant@users.noreply.github.com>
2023-12-22 11:24:02 -0500

    More width noneckpain

--- dotfiles/nvim/nvim/after/plugin/noneckpain.lua
+++ dotfiles/nvim/nvim/after/plugin/noneckpain.lua
@@ -1,4 +1,6 @@
-require("no-neck-pain").setup({})
+require("no-neck-pain").setup({
+  width = 120,
+})
 
 vim.keymap.set("n", "<leader>m", ":NoNeckPain<cr>",
   {silent = true, noremap = true, desc = "Toggle no neck pain"}
