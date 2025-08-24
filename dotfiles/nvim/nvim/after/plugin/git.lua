ae20f575d9bbd40a5c2bd854782ca70148454579
michaeldiamant <michaeldiamant@users.noreply.github.com>
2025-08-24 15:05:14 -0400

    nvim github commit/pr keybinds

--- dotfiles/nvim/nvim/after/plugin/git.lua
+++ dotfiles/nvim/nvim/after/plugin/git.lua
@@ -1,4 +1,3 @@
-
 require('gitsigns').setup {
   signs = {
     add          = { text = '+' },
@@ -82,3 +81,81 @@ require('gitsigns').setup {
   end
 }
 
+local function open_url(url)
+  -- Platform-specific command to open URL in default browser
+  local opener = "xdg-open" -- Linux, for macOS use 'open'
+  if vim.fn.has("macunix") == 1 then
+    opener = "open"
+  elseif vim.fn.has("win32") == 1 then
+    opener = "start"
+  end
+  os.execute(opener .. " " .. url)
+end
+
+local function get_git_blame_commit()
+  local line = vim.api.nvim_win_get_cursor(0)[1]
+  local file = vim.fn.expand("%")
+  local blame = vim.fn.systemlist("git blame -L " .. line .. "," .. line .. " --porcelain " .. file .. "| sed -n 's/^\\([0-9a-f]\\{40\\}\\).*/\\1/p' | uniq")
+  for _, l in ipairs(blame) do
+    local sha = string.match(l, "(%x+)")
+    if sha == "0000000000000000000000000000000000000000" then
+      return nil
+    elseif sha then
+      return sha
+    end
+  end
+  return nil
+end
+
+local function resolve_repo_name()
+  -- Get repo info: remote url -> parse to 'user/repo'
+  local remote_url = vim.fn.system("git config --get remote.origin.url"):gsub("\n", "")
+  local repo = nil
+  -- Convert git@github.com:user/repo.git or https://github.com/user/repo.git to user/repo
+  repo = remote_url:match("github.com[:/](.+)%.git")
+  return repo
+end
+
+local function open_github_commit()
+  local commit = get_git_blame_commit()
+  if not commit then
+    print("No commit found for current line")
+    return
+  end
+  local repo = resolve_repo_name()
+  if not repo then
+    print("Not a github.com repo")
+    return
+  end
+  local url = "https://github.com/" .. repo .. "/commit/" .. commit
+  open_url(url)
+end
+
+local function open_github_pr()
+  local commit = get_git_blame_commit()
+  if not commit then
+    print("No commit found for current line")
+    return
+  end
+
+  local repo = resolve_repo_name()
+  if not repo then
+    print("Not a github.com repo")
+    return
+  end
+
+  -- Using GitHub CLI (gh), get PR URL related to commit sha
+  local pr_url = vim.fn.system("gh api /repos/" .. repo .. "/commits/" .. commit .. "/pulls | jq -r 'if length > 0 then .[0].html_url else \"\" end'")
+  if pr_url == "" then
+    print("No PR found for commit")
+    return
+  end
+  open_url(pr_url)
+end
+
+vim.keymap.set('n', '<leader>gc',function()
+  open_github_commit()
+end, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gp',function()
+  open_github_pr()
+end, { noremap = true, silent = true })
