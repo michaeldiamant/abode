e32a1940e27d59ea6592b99ae0dfd8b889bbb0a3
michaeldiamant <michaeldiamant@users.noreply.github.com>
2026-08-02 21:28:02 -0400

    surfingkeys fix config

--- application-configs/surfingkeys.js
+++ application-configs/surfingkeys.js
@@ -3,8 +3,8 @@ api.map('gt', 'T');
 // Navigate tabs like vim windows
 api.map('<Ctrl-l>', 'E');
 api.map('<Ctrl-h>', 'R');
-api.map('<Ctrl-j>', 'E');
-api.map('<Ctrl-k>', 'R');
+api.map('<Ctrl-j>', 'R');
+api.map('<Ctrl-k>', 'E');
 
 settings.focusFirstCandidate = false;
 settings.tabsThreshold = 0;
