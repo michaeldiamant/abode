9940be00453213e6cdbba4c41e419e4cbe313435
michaeldiamant <michaeldiamant@users.noreply.github.com>
2026-09-16 11:43:21 -0400

    surfingkeys update

--- application-configs/surfingkeys.js
+++ application-configs/surfingkeys.js
@@ -8,16 +8,3 @@ api.map('<Ctrl-k>', 'E');
 
 settings.focusFirstCandidate = false;
 settings.tabsThreshold = 0;
-
-
-// name: Rosé Pine
-// author: thuanowa
-// license: unlicense
-// upstream: https://github.com/rose-pine/surfingkeys/blob/main/dist/rose-pine.conf
-// blurb: All natural pine, faux fur and a bit of soho vibes for the classy minimalist
-
-const hintsCss =
-  "font-size: 14pt; font-family: 'JetBrains Mono NL', 'Cascadia Code', 'Helvetica Neue', Helvetica, Arial, sans-serif; border: 0px; color: #e0def4 !important; background: #191724; background-color: #191724";
-
-api.Hints.style(hintsCss);
-api.Hints.style(hintsCss, "text");
