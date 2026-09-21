33a41bc0030b739add80fd225ced4c9e68b8171f
michaeldiamant <michaeldiamant@users.noreply.github.com>
2026-09-21 12:54:10 -0400

    python3 keymaps

--- dotfiles/nvim/nvim/lua/michaeldiamant/keymaps.lua
+++ dotfiles/nvim/nvim/lua/michaeldiamant/keymaps.lua
@@ -48,7 +48,7 @@ vim.keymap.set("n", "<C-s>l", ":vertical resize +3<CR>")
 vim.keymap.set("n", "gt", ":NERDTreeFind<CR><C-w>|")
 vim.keymap.set("n", "gT", ":NERDTree<CR><C-w>|")
 
-vim.keymap.set("n", "\\j", ":%! python -m json.tool<CR>")
+vim.keymap.set("n", "\\j", ":%! python3 -m json.tool<CR>")
 vim.keymap.set('n', '<leader>vl', function()
   local current = vim.diagnostic.config().virtual_lines
   vim.diagnostic.config({ virtual_lines = not current })
